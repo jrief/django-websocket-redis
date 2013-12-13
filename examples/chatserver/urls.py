3c0466c6658776bb36b83dc9792b1b285b274f52
Jacob Rief <jacob.rief@gmail.com>
2013-12-13 18:11:30 +0100

    websocket connects to the current port

--- examples/chatserver/urls.py
+++ examples/chatserver/urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, patterns
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 
 urlpatterns = patterns('',
-    url(r'^chat/$', lambda r: render_to_response('chat.html', { 'ws_url': 'ws://localhost:8000/ws/foobar' })),
+     url(r'^chat/$', lambda request: render(request, 'chat.html',
+         { 'ws_url': 'ws://localhost:{SERVER_PORT}/ws/foobar'.format(**request.META) })),
 )
