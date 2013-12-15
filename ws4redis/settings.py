9d0132c55cd75e0e31df121fae38fcb6afd2be18
Jacob Rief <jacob.rief@gmail.com>
2013-12-15 09:11:45 +0100

    Fixed settings string

--- ws4redis/settings.py
+++ ws4redis/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 
-REDIS_HOST = getattr(settings, 'WS4REDIS_HOST', 'localhost')
+REDIS_HOST = getattr(settings, 'REDIS_HOST', 'localhost')
 
-REDIS_PORT = getattr(settings, 'WS4REDIS_PORT', 6379)
+REDIS_PORT = getattr(settings, 'REDIS_PORT', 6379)
