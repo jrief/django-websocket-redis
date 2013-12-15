d833bb046499b74a6f1087db0ea2ec6aaf83cad3
Jacob Rief <jacob.rief@gmail.com>
2013-12-15 09:12:43 +0100

    work in progress for unit tests

--- examples/chatserver/tests/chatclient.py
+++ examples/chatserver/tests/chatclient.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+#from django.test.client import RequestFactory
+from django.test.client import Client
+from mock import patch
+from mockredis import mock_strict_redis_client
+
 
 
 class TestCase1(TestCase):
+    @patch('redis.StrictRedis', mock_strict_redis_client)
     def setUp(self):
         pass
 
     def test_1(self):
+        client = Client()
+        ws_http_headers = { 'HTTP_UPGRADE': 'upgrade' }
+        # , extra=ws_http_headers
+        request = client.request(ws_http_headers)
+        #print response.content
         self.assertTrue(True)
