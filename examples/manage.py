18c7f9c4edddea270ae15db762c20e1b8a008311
Jacob Rief <jacob.rief@gmail.com>
2013-12-18 18:00:49 +0100

    fixed include pathes

--- examples/manage.py
+++ examples/manage.py
@@ -2,10 +2,10 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('./../'))
+sys.path.insert(0, os.path.abspath('.'))
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', "chatserver.settings")
+    os.environ.update(DJANGO_SETTINGS_MODULE="chatserver.settings")
 
     from django.core.management import execute_from_command_line
 
