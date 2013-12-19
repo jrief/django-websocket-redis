a2ebc5171ab10d4c5a938e5cf4e1a37d6de7ce90
Jacob Rief <jacob.rief@gmail.com>
2013-12-19 09:37:51 +0100

    Fixed some setting for testing

--- examples/manage.py
+++ examples/manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 if __name__ == "__main__":
     os.environ.update(DJANGO_SETTINGS_MODULE="chatserver.settings")
