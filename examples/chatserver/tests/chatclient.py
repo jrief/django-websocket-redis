4a1bb9ee98291fa017a413e2e77e375436977e12
Jacob Rief <jacob.rief@gmail.com>
2013-12-21 22:13:58 +0100

    Adopting for Travis

--- examples/chatserver/tests/chatclient.py
+++ examples/chatserver/tests/chatclient.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 import time
 from django.test import LiveServerTestCase
-from mock import patch
 from websocket import create_connection
-from mockredis import mock_strict_redis_client
 from ws4redis.django_runserver import application
 
 
-#@patch('redis.StrictRedis', mock_strict_redis_client) # does not mock PUBSUB yet
 class WebsocketTests(LiveServerTestCase):
     fixtures = ['data.json']
 
