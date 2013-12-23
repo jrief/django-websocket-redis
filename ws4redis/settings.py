6bf009fcbd4bf47a1caea2060310c0ac075b68d7
Jacob Rief <jacob.rief@gmail.com>
2013-12-23 23:34:32 +0100

    Renamed settings.REDIS_... -> settings.WS4REDIR_...

--- ws4redis/settings.py
+++ ws4redis/settings.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 
-REDIS_HOST = getattr(settings, 'REDIS_HOST', 'localhost')
+WS4REDIS_HOST = getattr(settings, 'WS4REDIS_HOST', 'localhost')
 
-REDIS_PORT = getattr(settings, 'REDIS_PORT', 6379)
+WS4REDIS_PORT = getattr(settings, 'WS4REDIS_PORT', 6379)
+
+WS4REDIS_DB = getattr(settings, 'WS4REDIS_DB', 0)
+
+WS4REDIS_PASSWORD = getattr(settings, 'WS4REDIS_PASSWORD', None)
+
+WS4REDIS_EXPIRE = getattr(settings, 'WS4REDIS_EXPIRE', 0)
