d247666f75f7503660353c1577f2464f77881866
Jacob Rief <jacob.rief@gmail.com>
2013-12-25 18:28:50 +0100

    Added two additional tests

--- examples/chatserver/tests/chatclient.py
+++ examples/chatserver/tests/chatclient.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import time
+import redis
+from django.conf import settings
 from django.test import LiveServerTestCase
 from websocket import create_connection
+from ws4redis import settings as redis_settings
 from ws4redis.django_runserver import application
 
 
@@ -15,17 +18,42 @@ class WebsocketTests(LiveServerTestCase):
 
     def setUp(self):
         self.websocket_base_url = self.live_server_url.replace('http:', 'ws:', 1)
+        self.message = ''.join(unichr(c) for c in range(33, 128))
+        self.connection = redis.StrictRedis(**redis_settings.WS4REDIS_CONNECTION)
 
     @classmethod
     def tearDownClass(cls):
         time.sleep(1)
 
+    def test_pending_broadcast(self):
+        settings.WS4REDIS_EXPIRE = 10
+        self.connection.set('_broadcast_:foobar', self.message)
+        websocket_url = self.websocket_base_url + u'/ws/foobar?subscribe-broadcast'
+        ws = create_connection(websocket_url)
+        self.assertTrue(ws.connected)
+        result = ws.recv()
+        self.assertEqual(result, self.message)
+        ws.close()
+        self.assertFalse(ws.connected)
+
     def test_unified_broadcast(self):
+        settings.WS4REDIS_EXPIRE = 0
         websocket_url = self.websocket_base_url + u'/ws/foobar?subscribe-broadcast&publish-broadcast'
         ws = create_connection(websocket_url)
         self.assertTrue(ws.connected)
-        ws.send('Hello, World')
+        ws.send(self.message)
         result = ws.recv()
-        self.assertEqual(result, 'Hello, World')
+        self.assertEqual(result, self.message)
         ws.close()
         self.assertFalse(ws.connected)
+
+    def test_publish_broadcast(self):
+        settings.WS4REDIS_EXPIRE = 10
+        websocket_url = self.websocket_base_url + u'/ws/foobar?publish-broadcast'
+        ws = create_connection(websocket_url)
+        self.assertTrue(ws.connected)
+        ws.send(self.message)
+        ws.close()
+        self.assertFalse(ws.connected)
+        result = self.connection.get('_broadcast_:foobar')
+        self.assertEqual(result, self.message)
