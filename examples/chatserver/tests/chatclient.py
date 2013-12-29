65168210210ba969c1395f12af0a4dbed8eb0814
Jacob Rief <jacob.rief@gmail.com>
2013-12-29 11:20:53 +0100

    Added 5 unit tests and increased coverage

--- examples/chatserver/tests/chatclient.py
+++ examples/chatserver/tests/chatclient.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import time
 import redis
+import requests
 from django.conf import settings
 from django.test import LiveServerTestCase
+from django.core.urlresolvers import reverse
+from django.contrib.sessions.backends.db import SessionStore
 from websocket import create_connection
 from ws4redis import settings as redis_settings
 from ws4redis.django_runserver import application
@@ -25,7 +28,7 @@ class WebsocketTests(LiveServerTestCase):
     def tearDownClass(cls):
         time.sleep(1)
 
-    def test_pending_broadcast(self):
+    def test_subscribe_broadcast(self):
         settings.WS4REDIS_EXPIRE = 10
         self.connection.set('_broadcast_:foobar', self.message)
         websocket_url = self.websocket_base_url + u'/ws/foobar?subscribe-broadcast'
@@ -36,7 +39,7 @@ class WebsocketTests(LiveServerTestCase):
         ws.close()
         self.assertFalse(ws.connected)
 
-    def test_unified_broadcast(self):
+    def test_pubsub_broadcast(self):
         settings.WS4REDIS_EXPIRE = 0
         websocket_url = self.websocket_base_url + u'/ws/foobar?subscribe-broadcast&publish-broadcast'
         ws = create_connection(websocket_url)
@@ -57,3 +60,78 @@ class WebsocketTests(LiveServerTestCase):
         self.assertFalse(ws.connected)
         result = self.connection.get('_broadcast_:foobar')
         self.assertEqual(result, self.message)
+
+    def test_subscribe_user(self):
+        logged_in = self.client.login(username='admin', password='secret')
+        self.assertTrue(logged_in, 'User is not logged in')
+        settings.WS4REDIS_EXPIRE = 10
+        self.connection.set('admin:foobar', self.message)
+        websocket_url = self.websocket_base_url + u'/ws/foobar?subscribe-user'
+        header = ['Cookie: sessionid={0}'.format(self.client.cookies['sessionid'].coded_value)]
+        ws = create_connection(websocket_url, header=header)
+        self.assertTrue(ws.connected)
+        result = ws.recv()
+        self.assertEqual(result, self.message)
+        ws.close()
+        self.assertFalse(ws.connected)
+
+    def test_publish_user(self):
+        logged_in = self.client.login(username='admin', password='secret')
+        self.assertTrue(logged_in, 'User is not logged in')
+        settings.WS4REDIS_EXPIRE = 10
+        websocket_url = self.websocket_base_url + u'/ws/foobar?publish-user'
+        header = ['Cookie: sessionid={0}'.format(self.client.cookies['sessionid'].coded_value)]
+        ws = create_connection(websocket_url, header=header)
+        self.assertTrue(ws.connected)
+        ws.send(self.message)
+        ws.close()
+        self.assertFalse(ws.connected)
+        result = self.connection.get('admin:foobar')
+        self.assertEqual(result, self.message)
+
+    def test_subscribe_session(self):
+        response = self.client.get(reverse('admin:index'))
+        self.assertIsInstance(response.client.session, SessionStore, 'Did not receive a sessionid')
+        session_key = response.client.session.session_key
+        self.assertGreater(len(session_key), 30, 'Session key is too short')
+        settings.WS4REDIS_EXPIRE = 10
+        self.connection.set('{0}:foobar'.format(session_key), self.message)
+        websocket_url = self.websocket_base_url + u'/ws/foobar?subscribe-session'
+        header = ['Cookie: sessionid={0}'.format(session_key)]
+        ws = create_connection(websocket_url, header=header)
+        self.assertTrue(ws.connected)
+        result = ws.recv()
+        self.assertEqual(result, self.message)
+        ws.close()
+        self.assertFalse(ws.connected)
+
+    def test_publish_session(self):
+        response = self.client.get(reverse('admin:index'))
+        self.assertIsInstance(response.client.session, SessionStore, 'Did not receive a sessionid')
+        session_key = response.client.session.session_key
+        self.assertGreater(len(session_key), 30, 'Session key is too short')
+        settings.WS4REDIS_EXPIRE = 10
+        websocket_url = self.websocket_base_url + u'/ws/foobar?publish-session'
+        header = ['Cookie: sessionid={0}'.format(session_key)]
+        ws = create_connection(websocket_url, header=header)
+        self.assertTrue(ws.connected)
+        ws.send(self.message)
+        ws.close()
+        self.assertFalse(ws.connected)
+        result = self.connection.get('{0}:foobar'.format(session_key))
+        self.assertEqual(result, self.message)
+
+    def test_invalid_request(self):
+        websocket_url = self.live_server_url + u'/ws/foobar'
+        response = requests.get(websocket_url)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('upgrade to a websocket', response.content)
+        response = requests.post(websocket_url, {})
+        self.assertEqual(response.status_code, 400)
+
+    def t_e_s_t_invalid_version(self):
+        # does not work: websocket library overrides Sec-WebSocket-Version
+        websocket_url = self.websocket_base_url + u'/ws/foobar?publish-broadcast'
+        header = ['Sec-WebSocket-Version: 6']  # Version 6 is not supported
+        ws = create_connection(websocket_url, header=header)
+        self.assertFalse(ws.connected)
