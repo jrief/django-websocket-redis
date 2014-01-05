791e7788c1136fe8a7ed844441df9570a9cd0542
Jacob Rief <jacob.rief@gmail.com>
2014-01-05 08:54:54 +0100

    Moved monkey patching redis socket into runner

--- stress-tests/wsgi_websocket.py
+++ stress-tests/wsgi_websocket.py
@@ -2,8 +2,12 @@
 # uwsgi --virtualenv /path/to/virtualenv --http-socket /var/tmp/web.socket --gevent 10000 --umask 000 --http-websockets --master --workers 2 --module wsgi_websocket
 import os
 import sys
+import gevent.monkey
+import redis.connection
+redis.connection.socket = gevent.socket
+
 sys.path[0:0] = [os.path.abspath('..'), os.path.abspath('../examples')]
 os.environ.update(DJANGO_SETTINGS_MODULE='chatserver.settings')
-from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 
+from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 application = uWSGIWebsocketServer()
