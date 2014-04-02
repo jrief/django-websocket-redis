cbdfe93f4f8c05bb75e4c35bfb21dab95bcdb264
Jacob Rief <jacob.rief@gmail.com>
2014-04-02 23:17:33 +0200

    check for pandoc in PATH

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ CLASSIFIERS = [
 
 def read(fname):
     readme_file = os.path.join(os.path.dirname(__file__), fname)
-    return os.popen('pandoc -t rst {0}'.format(readme_file)).read()
+    return os.popen('which pandoc && pandoc -t rst {0} || cat {0}'.format(readme_file)).read()
 
 
 setup(
@@ -43,7 +43,8 @@ setup(
         'greenlet',
     ],
     extras_require={
-        'uwsgi':  ['uWSGI>=1.9.20'],
+        'uwsgi': ['uWSGI>=1.9.20'],
         'wsaccel': ['wsaccel>=0.6.2'],
+        'django-redis-sessions': ['django-redis-sessions>=0.4.0'],
     },
 )
