5aedd3a0ec213870ba0dacf1b1061b1f0a0796b7
Jacob Rief <jacob.rief@gmail.com>
2014-04-03 14:03:36 +0200

    Adopted unit tests for group publish/submission

--- examples/chatserver/tests/chatclient.py
+++ examples/chatserver/tests/chatclient.py
@@ -8,6 +8,8 @@ from django.contrib.sessions.backends.db import SessionStore
 from websocket import create_connection
 from ws4redis.django_runserver import application
 from ws4redis.publisher import RedisPublisher
+from ws4redis.subscriber import RedisSubscriber
+from ws4redis.redis_store import SELF
 
 
 class WebsocketTests(LiveServerTestCase):
@@ -19,11 +21,10 @@ class WebsocketTests(LiveServerTestCase):
         cls.server_thread.httpd.set_app(application)
 
     def setUp(self):
-        self.websocket_base_url = self.live_server_url.replace('http:', 'ws:', 1)
+        self.facility = u'unittest'
+        self.websocket_base_url = self.live_server_url.replace('http:', 'ws:', 1) + u'/ws/' + self.facility
         self.message = ''.join(unichr(c) for c in range(33, 128))
-        self.adminuser = User.objects.get(username='admin')
         self.factory = RequestFactory()
-        self.facility = 'foobar'
 
     @classmethod
     def tearDownClass(cls):
@@ -33,7 +34,7 @@ class WebsocketTests(LiveServerTestCase):
         audience = {'broadcast': True}
         publisher = RedisPublisher(facility=self.facility, **audience)
         publisher.publish_message(self.message, 10)
-        websocket_url = self.websocket_base_url + u'/ws/foobar?subscribe-broadcast'
+        websocket_url = self.websocket_base_url + u'?subscribe-broadcast'
         ws = create_connection(websocket_url)
         self.assertTrue(ws.connected)
         result = ws.recv()
@@ -42,7 +43,7 @@ class WebsocketTests(LiveServerTestCase):
         self.assertFalse(ws.connected)
 
     def test_pubsub_broadcast(self):
-        websocket_url = self.websocket_base_url + u'/ws/foobar?subscribe-broadcast&publish-broadcast'
+        websocket_url = self.websocket_base_url + u'?subscribe-broadcast&publish-broadcast'
         ws = create_connection(websocket_url)
         self.assertTrue(ws.connected)
         ws.send(self.message)
@@ -52,7 +53,7 @@ class WebsocketTests(LiveServerTestCase):
         self.assertFalse(ws.connected)
 
     def test_publish_broadcast(self):
-        websocket_url = self.websocket_base_url + u'/ws/foobar?publish-broadcast'
+        websocket_url = self.websocket_base_url + u'?publish-broadcast'
         ws = create_connection(websocket_url)
         self.assertTrue(ws.connected)
         ws.send(self.message)
@@ -63,17 +64,17 @@ class WebsocketTests(LiveServerTestCase):
         result = publisher.fetch_message(request, self.facility, 'broadcast')
         self.assertEqual(result, self.message)
         # now access Redis store directly
-        self.assertEqual(publisher._connection.get('ws4redis:broadcast:foobar'), self.message)
+        self.assertEqual(publisher._connection.get('ws4redis:broadcast:' + self.facility), self.message)
 
     def test_subscribe_user(self):
-        logged_in = self.client.login(username='admin', password='secret')
-        self.assertTrue(logged_in, 'User is not logged in')
+        logged_in = self.client.login(username='john', password='secret')
+        self.assertTrue(logged_in, 'John is not logged in')
         request = self.factory.get('/chat/')
-        request.user = self.adminuser
-        audience = {'users': True}
+        request.user = User.objects.get(username='mary')
+        audience = {'users': ['john', 'mary']}
         publisher = RedisPublisher(request=request, facility=self.facility, **audience)
         publisher.publish_message(self.message, 10)
-        websocket_url = self.websocket_base_url + u'/ws/foobar?subscribe-user'
+        websocket_url = self.websocket_base_url + u'?subscribe-user'
         header = ['Cookie: sessionid={0}'.format(self.client.cookies['sessionid'].coded_value)]
         ws = create_connection(websocket_url, header=header)
         self.assertTrue(ws.connected)
@@ -83,9 +84,9 @@ class WebsocketTests(LiveServerTestCase):
         self.assertFalse(ws.connected)
 
     def test_publish_user(self):
-        logged_in = self.client.login(username='admin', password='secret')
-        self.assertTrue(logged_in, 'User is not logged in')
-        websocket_url = self.websocket_base_url + u'/ws/foobar?publish-user'
+        logged_in = self.client.login(username='john', password='secret')
+        self.assertTrue(logged_in, 'John is not logged in')
+        websocket_url = self.websocket_base_url + u'?publish-user'
         header = ['Cookie: sessionid={0}'.format(self.client.cookies['sessionid'].coded_value)]
         ws = create_connection(websocket_url, header=header)
         self.assertTrue(ws.connected)
@@ -94,22 +95,61 @@ class WebsocketTests(LiveServerTestCase):
         self.assertFalse(ws.connected)
         publisher = RedisPublisher()
         request = self.factory.get('/chat/')
-        request.user = self.adminuser
+        request.user = User.objects.get(username='john')
         result = publisher.fetch_message(request, self.facility, 'user')
+        self.assertEqual(result, self.message)
+        request.user = User.objects.get(username='mary')
+        result = publisher.fetch_message(request, self.facility, 'user')
+        self.assertEqual(result, None)
+
+    def test_subscribe_group(self):
+        logged_in = self.client.login(username='john', password='secret')
+        self.assertTrue(logged_in, 'John is not logged in')
+        request = self.factory.get('/chat/')
+        request.user = User.objects.get(username='mary')
+        audience = {'groups': ['chatters']}
+        publisher = RedisPublisher(request=request, facility=self.facility, **audience)
+        publisher.publish_message(self.message, 10)
+        websocket_url = self.websocket_base_url + u'?subscribe-group'
+        header = ['Cookie: sessionid={0}'.format(self.client.cookies['sessionid'].coded_value)]
+        ws = create_connection(websocket_url, header=header)
+        self.assertTrue(ws.connected)
+        result = ws.recv()
+        self.assertEqual(result, self.message)
+        ws.close()
+        self.assertFalse(ws.connected)
+
+    def test_publish_group(self):
+        logged_in = self.client.login(username='john', password='secret')
+        self.assertTrue(logged_in, 'John is not logged in')
+        websocket_url = self.websocket_base_url + u'?publish-group'
+        header = ['Cookie: sessionid={0}'.format(self.client.cookies['sessionid'].coded_value)]
+        ws = create_connection(websocket_url, header=header)
+        self.assertTrue(ws.connected)
+        ws.send(self.message)
+        ws.close()
+        self.assertFalse(ws.connected)
+        publisher = RedisPublisher()
+        request = self.factory.get('/chat/')
+        request.user = User.objects.get(username='mary')
+        logged_in = self.client.login(username='mary', password='secret')
+        self.assertTrue(logged_in, 'Mary is not logged in')
+        request.session = self.client.session
+        result = publisher.fetch_message(request, self.facility, 'group')
         self.assertEqual(result, self.message)
 
     def test_subscribe_session(self):
-        logged_in = self.client.login(username='admin', password='secret')
-        self.assertTrue(logged_in, 'User is not logged in')
-        self.assertIsInstance(self.client.session, (dict, SessionStore), 'Did not receive a sessionid')
+        logged_in = self.client.login(username='john', password='secret')
+        self.assertTrue(logged_in, 'John is not logged in')
+        self.assertIsInstance(self.client.session, (dict, SessionStore), 'Did not receive a session key')
         session_key = self.client.session.session_key
         self.assertGreater(len(session_key), 30, 'Session key is too short')
         request = self.factory.get('/chat/')
         request.session = self.client.session
-        audience = {'sessions': True}
+        audience = {'sessions': [SELF]}
         publisher = RedisPublisher(request=request, facility=self.facility, **audience)
         publisher.publish_message(self.message, 10)
-        websocket_url = self.websocket_base_url + u'/ws/foobar?subscribe-session'
+        websocket_url = self.websocket_base_url + u'?subscribe-session'
         header = ['Cookie: sessionid={0}'.format(session_key)]
         ws = create_connection(websocket_url, header=header)
         self.assertTrue(ws.connected)
@@ -119,12 +159,12 @@ class WebsocketTests(LiveServerTestCase):
         self.assertFalse(ws.connected)
 
     def test_publish_session(self):
-        logged_in = self.client.login(username='admin', password='secret')
-        self.assertTrue(logged_in, 'User is not logged in')
-        self.assertIsInstance(self.client.session, (dict, SessionStore), 'Did not receive a sessionid')
+        logged_in = self.client.login(username='mary', password='secret')
+        self.assertTrue(logged_in, 'Mary is not logged in')
+        self.assertIsInstance(self.client.session, (dict, SessionStore), 'Did not receive a session key')
         session_key = self.client.session.session_key
         self.assertGreater(len(session_key), 30, 'Session key is too short')
-        websocket_url = self.websocket_base_url + u'/ws/foobar?publish-session'
+        websocket_url = self.websocket_base_url + u'?publish-session'
         header = ['Cookie: sessionid={0}'.format(session_key)]
         ws = create_connection(websocket_url, header=header)
         self.assertTrue(ws.connected)
@@ -147,13 +187,13 @@ class WebsocketTests(LiveServerTestCase):
 
     def t_e_s_t_invalid_version(self):
         # does not work: websocket library overrides Sec-WebSocket-Version
-        websocket_url = self.websocket_base_url + u'/ws/foobar?publish-broadcast'
+        websocket_url = self.websocket_base_url + u'?publish-broadcast'
         header = ['Sec-WebSocket-Version: 6']  # Version 6 is not supported
         ws = create_connection(websocket_url, header=header)
         self.assertFalse(ws.connected)
 
     def test_defining_multiple_publishers(self):
         pub1 = RedisPublisher(facility=self.facility, broadcast=True)
-        self.assertEqual(pub1._publishers, set(['ws4redis:broadcast:foobar']))
-        pub2 = RedisPublisher(facility=self.facility, users='admin')
-        self.assertEqual(pub2._publishers, set(['ws4redis:user:admin:foobar']))
+        self.assertEqual(pub1._publishers, set(['ws4redis:broadcast:' + self.facility]))
+        pub2 = RedisPublisher(facility=self.facility, users=['john'])
+        self.assertEqual(pub2._publishers, set(['ws4redis:user:john:' + self.facility]))
