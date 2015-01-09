cd5577f1cde692fdd975cbaa733bed80dc59c9e2
Jacob Rief <jacob.rief@gmail.com>
2015-01-09 16:45:53 +0100

    Message is sent on carriage return

--- examples/chatserver/urls.py
+++ examples/chatserver/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, patterns, include
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import RedirectView
 from django.contrib import admin
 from .views import BroadcastChatView, UserChatView, GroupChatView
@@ -14,4 +15,4 @@ urlpatterns = patterns('',
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', RedirectView.as_view(url=reverse_lazy('broadcast_chat'))),
-)
+) + staticfiles_urlpatterns()
