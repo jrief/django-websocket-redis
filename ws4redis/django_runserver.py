9c03e0472b2cf88db898c64ead6e0848a5456f90
Erwin Junge <erwin@junge.nl>
2016-01-15 17:33:48 +0100

    Fix Sec-WebSocket-Protocol header appending

--- ws4redis/django_runserver.py
+++ ws4redis/django_runserver.py
@@ -52,7 +52,7 @@ class WebsocketRunServer(WebsocketWSGIServer):
             ('Sec-WebSocket-Version', str(websocket_version))
         ]
         if environ.get('HTTP_SEC_WEBSOCKET_PROTOCOL') is not None:
-            headers.append('Sec-WebSocket-Protocol', environ.get('HTTP_SEC_WEBSOCKET_PROTOCOL'))
+            headers.append(('Sec-WebSocket-Protocol', environ.get('HTTP_SEC_WEBSOCKET_PROTOCOL')))
 
         logger.debug('WebSocket request accepted, switching protocols')
         start_response(force_str('101 Switching Protocols'), headers)
