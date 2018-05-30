cbb34c6a17ab89c84d8b5b77ab7d7f8d15871cc5
Jacob Rief <jacob.rief@gmail.com>
2018-05-30 21:23:16 +0200

    fix Django-1.11 incompatibilities

--- examples/chatserver/urls.py
+++ examples/chatserver/urls.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, patterns, include
+from django import VERSION as DJANGO_VERSION
+from django.conf.urls import include
+if DJANGO_VERSION < (1, 11):
+    from django.conf.urls import url, patterns
+else:
+    from django.urls import url
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import RedirectView
@@ -8,11 +13,21 @@ from .views import BroadcastChatView, UserChatView, GroupChatView
 admin.autodiscover()
 
 
-urlpatterns = patterns('',
-    url(r'^chat/$', BroadcastChatView.as_view(), name='broadcast_chat'),
-    url(r'^userchat/$', UserChatView.as_view(), name='user_chat'),
-    url(r'^groupchat/$', GroupChatView.as_view(), name='group_chat'),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('broadcast_chat'))),
-) + staticfiles_urlpatterns()
+if DJANGO_VERSION < (1, 11):
+    urlpatterns = patterns('',
+        url(r'^chat/$', BroadcastChatView.as_view(), name='broadcast_chat'),
+        url(r'^userchat/$', UserChatView.as_view(), name='user_chat'),
+        url(r'^groupchat/$', GroupChatView.as_view(), name='group_chat'),
+        url(r'^accounts/', include('django.contrib.auth.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'^$', RedirectView.as_view(url=reverse_lazy('broadcast_chat'))),
+    ) + staticfiles_urlpatterns()
+else:
+    urlpatterns = [
+        url(r'^chat/$', BroadcastChatView.as_view(), name='broadcast_chat'),
+        url(r'^userchat/$', UserChatView.as_view(), name='user_chat'),
+        url(r'^groupchat/$', GroupChatView.as_view(), name='group_chat'),
+        url(r'^accounts/', include('django.contrib.auth.urls')),
+        url(r'^admin/', include(admin.site.urls)),
+        url(r'^$', RedirectView.as_view(url=reverse_lazy('broadcast_chat'))),
+    ]
