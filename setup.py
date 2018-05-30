1d4032518b661c9791ee418b77ad685c0ee83fa0
Jacob Rief <jacob.rief@gmail.com>
2018-05-30 20:05:45 +0200

    attempt to run tests with Python 3.4-3.6

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
 ]
 
 setup(
