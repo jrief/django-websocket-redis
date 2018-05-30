c79d6e55bba3cc9a361eb575266b5270b6e35317
Jacob Rief <jacob.rief@gmail.com>
2018-05-30 23:50:19 +0200

    attempt to fix tests for Django-2

--- examples/chatserver/urls.py
+++ examples/chatserver/urls.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from django import VERSION as DJANGO_VERSION
-from django.conf.urls import include
 if DJANGO_VERSION < (1, 10):
-    from django.conf.urls import url, patterns
+    from django.conf.urls import url, patterns, include
     from django.core.urlresolvers import reverse_lazy
+elif DJANGO_VERSION < 2:
+    from django.conf.urls import url, include
+    from django.urls import reverse_lazy
 else:
-    from django.conf.urls import url
+    from django.urls import path, include
     from django.urls import reverse_lazy
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import RedirectView
@@ -23,7 +25,7 @@ if DJANGO_VERSION < (1, 10):
         url(r'^admin/', include(admin.site.urls)),
         url(r'^$', RedirectView.as_view(url=reverse_lazy('broadcast_chat'))),
     ) + staticfiles_urlpatterns()
-else:
+elif DJANGO_VERSION < 2:
     urlpatterns = [
         url(r'^chat/$', BroadcastChatView.as_view(), name='broadcast_chat'),
         url(r'^userchat/$', UserChatView.as_view(), name='user_chat'),
@@ -31,4 +33,13 @@ else:
         url(r'^accounts/', include('django.contrib.auth.urls')),
         url(r'^admin/', include(admin.site.urls)),
         url(r'^$', RedirectView.as_view(url=reverse_lazy('broadcast_chat'))),
+    ]
+else:
+    urlpatterns = [
+        path('chat/', BroadcastChatView.as_view(), name='broadcast_chat'),
+        path('userchat/', UserChatView.as_view(), name='user_chat'),
+        path('groupchat/', GroupChatView.as_view(), name='group_chat'),
+        path('accounts/', include('django.contrib.auth.urls')),
+        path('admin/', include(admin.site.urls)),
+        path('', RedirectView.as_view(url=reverse_lazy('broadcast_chat'))),
     ]
