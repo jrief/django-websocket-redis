0aba6ca3bfa9b8acb0624143822519bc4a0e5b96
silent <sezonov.daniil@gmail.com>
2019-02-05 15:56:13 +0300

    Refactoring for Django minor versions.

--- ws4redis/django_runserver.py
+++ ws4redis/django_runserver.py
@@ -95,7 +95,7 @@ class WebsocketRunServer(WebsocketWSGIServer):
         logger.debug('WebSocket request accepted, switching protocols')
         start_response(force_str('101 Switching Protocols'), headers)
         six.get_method_self(start_response).finish_content()
-        if (django.VERSION[:3] > (2,1,4)):
+        if (django.VERSION[:3] >= (2,1,5)):
             wsgi_input = environ['wsgi.input'].stream
         else:
             wsgi_input = environ['wsgi.input']
