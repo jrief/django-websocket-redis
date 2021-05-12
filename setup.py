772d9ebd41d5d0712f58f6861284b4f3d6b2298c
Goran Pavlovic <gpavlovic@apple.com>
2021-05-12 13:18:54 +0200

    Modified setup.py

--- setup.py
+++ setup.py
@@ -34,8 +34,6 @@ CLASSIFIERS = [
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Development Status :: 4 - Beta',
     'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
